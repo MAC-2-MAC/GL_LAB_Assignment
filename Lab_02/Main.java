51a37c319f2776c20b7e891a4995776507193149
mehdi812655 <106870262+mehdi812655@users.noreply.github.com>
2022-07-01 23:18:50 +0530

    Add files via upload

---
+++ Lab_02/Main.java
@@ -0,0 +1,20 @@
+package com.Lab_02;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+    
+        AdminDepartment AdminDeptData = new AdminDepartment();
+        AdminDeptData.AdminDeptData();
+     
+	   HrDepartment HrDeptData = new HrDepartment();
+	   HrDeptData.HrDeptData();
+	   
+	   TechDepartment TechDeptData = new TechDepartment();
+	   TechDeptData.TechDeptData();
+	   
+	}
+	 
+	   
+}
